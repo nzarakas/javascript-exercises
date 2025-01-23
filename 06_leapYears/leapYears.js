b2d293b5c4c44fadfacddb34419767c7cff86153
Nicolas Zarakas <nzarakas1@gmail.com>
2025-01-23 21:26:50 +0200

    my solution to 06 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(!Number.isInteger(year)) return `ERROR`;
 
+    if(year%4===0 && year%100!==0) return true;
+         
+    if(year%400===0) return true;
+
+    return false;
 };
 
 // Do not edit below this line
